dea0d933bf482259bcef3564c7e548289f627bef
Nathan Hurst <nathanhurst03@gmail.com>
2021-07-01 21:05:34 -0600

    Move ViGEmBus check to Program.cs Check for ViGEmBus before the main window is first created.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Nefarius.ViGEm.Client;
 
 namespace Program
 {
@@ -11,14 +12,26 @@ namespace Program
 		/// <summary>
 		///  The main entry point for the application.
 		/// </summary>
-
 		[STAThread]
 		static void Main()
 		{
+			bool vigem;
+			try
+            {
+                var client = new ViGEmClient();
+                client.Dispose();
+				vigem = true;
+            }
+			catch (Nefarius.ViGEm.Client.Exceptions.VigemBusNotFoundException)
+            {
+                MessageBox.Show("ViGEmBus was not found on the system./r/nXbox 360 controller output functionality will be unavailable until you install it.", "ViGEmBus not found", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                vigem = false;
+            }
+
 			Application.SetHighDpiMode(HighDpiMode.SystemAware);
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new MainWindow());
+			Application.Run(new MainWindow(vigem));
 		}
 	}
 }
